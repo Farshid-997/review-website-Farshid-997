67f9cb2acb135e837e5609ce693a30c971bab9e4
Farshid Ahsan <arnob.farshid@gmail.com>
2021-10-05 15:42:07 +0600

    updated one

--- src/component/Cards/DisplayCards.js
+++ src/component/Cards/DisplayCards.js
@@ -9,18 +9,16 @@ const DisplayCards = (props) => {
     return (
         <div className="Cards">
 
-            <Card style={{ width: '18rem', color: 'black', margin: '5px', padding: "3px", boxShadow: '5px 5px  #aaaaaa' }}>
-                <Card.Img variant="top" src={image} style={{ width: '250px', height: '250px', margin: '5px', padding: '5px', borderRadius: '10px' }} />
+            <Card style={{ width: '18rem', color: 'black', margin: '5px', padding: "3px", boxShadow: '5px 5px  #aaaaaa', height: '450px' }}>
+                <Card.Img variant="top" src={image} style={{ width: '250px', height: '250px', margin: '5px', padding: '5px', borderRadius: '5px', textAlign: 'center' }} />
                 <Card.Body>
                     <Card.Title>Language:{LanguageName}</Card.Title>
                     <Card.Text>
-                        <h4>{InstructorName}</h4>
-                        <p>{totalStudentEnroll}</p>
-                        <p>{price}</p>
+                        <h4>Instructor Name:{InstructorName}</h4>
+                        <p>Total Student Enroll:{totalStudentEnroll}</p>
+                        <p style={{ color: "blue", fontWeight: 'bold' }}>Price:{price}Tk</p>
                     </Card.Text>
-                    <footer >
-                        <p>All have Limited Offer</p>
-                    </footer>
+
                 </Card.Body>
             </Card>
 
--- src/component/showHomeCard/ShowCard.js
+++ src/component/showHomeCard/ShowCard.js
@@ -5,14 +5,14 @@ const ShowCard = (props) => {
     const { LanguageName, totalStudentEnroll, price, InstructorName, image } = props.cd
     return (
         <div>
-            <Card style={{ width: '18rem', color: 'black', margin: '5px', padding: "5px" }}>
+            <Card style={{ width: '18rem', color: 'black', margin: '5px', padding: "5px", height: '400px' }}>
                 <Card.Img variant="top" src={image} style={{ width: '250px', height: '250px', margin: '5px', padding: '5px', borderRadius: '10px' }} />
                 <Card.Body>
                     <Card.Title>Language:{LanguageName}</Card.Title>
                     <Card.Text>
-                        <h4>{InstructorName}</h4>
-                        <p>{totalStudentEnroll}</p>
-                        <p>{price}</p>
+                        <h4>Instructor Name:{InstructorName}</h4>
+                        <p>Total Student Enroll:{totalStudentEnroll}</p>
+                        <p style={{ color: "blue", fontWeight: 'bold' }}>Price:{price}Tk.</p>
                     </Card.Text>
 
                 </Card.Body>
